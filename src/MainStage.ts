dfb61fe492b8a3720c1399fc03b3be946af67ad4
arsenm-dc <arsenm@doublecoconut.com>
2025-08-30 18:22:50 +0400

    Removed bkg view

--- src/MainStage.ts
+++ src/MainStage.ts
@@ -1,9 +1,7 @@
 import { Container } from 'pixi.js';
-import { BackgroundView } from './views/BackgroundView';
 import { GameView } from './views/GameView';
 
 class PixiStage extends Container {
-    private bgView: BackgroundView;
     private gameView: GameView;
 
     constructor() {
@@ -11,13 +9,10 @@ class PixiStage extends Container {
     }
 
     public resize(): void {
-        this.bgView?.rebuild();
         this.gameView?.rebuild();
     }
 
     public start(): void {
-        this.bgView = new BackgroundView();
-        this.addChild(this.bgView);
         this.gameView = new GameView();
         this.addChild(this.gameView);
     }
--- src/views/BoardView.ts
+++ src/views/BoardView.ts
@@ -1,6 +1,4 @@
-import { Spine } from 'pixi-spine';
-import { Assets, Container } from 'pixi.js';
-import { spines } from '../assets/assetsNames/spines';
+import { Container, Rectangle, Texture, TilingSprite } from 'pixi.js';
 
 export class BoardView extends Container {
     constructor() {
@@ -8,18 +6,34 @@ export class BoardView extends Container {
         this.build();
     }
 
+    public getBounds(skipUpdate?: boolean, rect?: Rectangle): Rectangle {
+        return new Rectangle(0, 0, 2048, 1890);
+    }
+
     private build(): void {
-        const spin = Assets.cache.get(spines[0].jsonURL).spineData;
-        const spine: Spine = new Spine(spin);
+        this.buildSky();
+        // const spin = Assets.cache.get(spines[0].jsonURL).spineData;
+        // const spine: Spine = new Spine(spin);
 
-        // 'Death start'
-        // 'Falling'
-        // 'Falling for death'
-        // 'Landing'
-        // 'Swing'
-        // 'spike death'
+        // // 'Death start'
+        // // 'Falling'
+        // // 'Falling for death'
+        // // 'Landing'
+        // // 'Swing'
+        // // 'spike death'
 
-        spine.state.setAnimation(0, 'spike death', true);
-        this.addChild(spine);
+        // spine.state.setAnimation(0, 'spike death', true);
+        // this.addChild(spine);
+    }
+
+    private buildSky(): void {
+        const texture = Texture.from('sky.png');
+        const sky = new TilingSprite(texture, 4000, 2000);
+        this.addChild(sky);
+
+        // const texture2 = Texture.from('stars.png');
+        // const stars = new TilingSprite(texture2, 1000, 500);
+        // stars.scale.set(3);
+        // this.addChild(stars);
     }
 }
